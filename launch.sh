530b5e2413b0ec9374a5d0e4c7251740bf8fb174
Ankit Karna <ankitkarna@lftechnology.com>
2026-04-03 21:11:47 +0545

    Add migrations to launch script (#5)

--- launch.sh
+++ launch.sh
@@ -4,4 +4,6 @@ set -e
 echo "Starting Synapse Backend API..."
 
 # Run application with envault to inject secrets
+alembic revision --autogenerate -m "$(msg)"
+alembic upgrade head
 exec envault run -e assetguard "uvicorn app.main:app --host 0.0.0.0 --port 8000"
