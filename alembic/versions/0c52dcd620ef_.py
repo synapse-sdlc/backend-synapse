ae497cf7116fc9e417a92f30a22a895f116e6c77
Sushant Shrestha <25529240+su-sh@users.noreply.github.com>
2026-04-04 20:25:40 +0545

    Fix branched migration: rechain 0c52dcd620ef to depend on 0005 instead of 0004

--- alembic/versions/0c52dcd620ef_.py
+++ alembic/versions/0c52dcd620ef_.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '0c52dcd620ef'
-down_revision: Union[str, None] = '0004'
+down_revision: Union[str, None] = '0005'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
